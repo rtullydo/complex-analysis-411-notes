bad0f700d82cb211d0fc15cd04084076fa784297
rtullydo <rtullydo@gmail.com>
2023-01-21 13:37:12 -0800

    update

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -142,7 +142,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.2",
   "title": "Simply connected sets, boundaries, and the Riemann mapping theorem",
-  "body": " Simply connected sets, boundaries, and the Riemann mapping theorem  Before we go on, we're going to need to deal with the issue that analytic maps don't always act nicely on the boundaries of their domains. (Remember, analytic and holomorphic are local properties defined on open sets, and the edge of a set isn't open.) Let us recall that a set is called simply connected if it has no holes in it . This intuitive idea of no holes corresponds to the rigorous notion a simply connected set is one in which every simple closed curve can be continuously deformed into a point without leaving the set. The act of continuously deforming a curve within a set is called homotopy . Two curves are said to be homotopic if we can find a homotopy from one curve to the other. More precisely, a homotopy in a domain from a curve to a a curve is a continuous function where for all , and the endpoints have , and .  Two domains are said to be conformally equivalent if there exists a bijective conformal map . The next theorem characterizes which sets are conformally equivalent to simply connected sets.   Riemann Mapping Theorem  Let be a simply connected domain in with . Then there exists a bijective conformal map , where is the complex unit disk. For any fixed , we can find a unique such so that and .   Because the maps in the theorem above are bijections, we get a striking immediate corollary.   Any two simply connected regions, neither one of which is all of , are conformally equivalent.   This is a powerful fact! Unfortunately, this theorem doesn't tell us how to fund the conformal maps that connect different sets. We'll take a deeper dive into that in the next couple of lectures.  Now, we'll turn to the question of what happens to the boundaries of domains under conformal maps. One way to think of the boundary of a domain in is the set of points that can be reached as limits of sequences in . Another is to think of it as the set of points for which any neighborhood contains points in and points outside of . We use the notation to indicated the boundary of . Because domains in are open, a set and its boundary are disjoint. The closure of a domain is and will be denoted (we can also use , but to avoid confusion with the complex conjugate, we'll avoid that in these notes).   Let be domains in with boundaries . Suppose that is conformal (that is, is conformal from its domain to its range). If has boundary and additionally for some , then .   Essentially, the previous proposition implies that we can find the image of a conformal map by computing the image of the boundary and keeping track of where the interior of the domain was mapped via a single point.  We now come to the question of when a conformal map between domains can be extended to the boundary. Note that this extension will be a continuous extension, not an analytic one, because we can't work on neighborhoods for points in the boundary. For nice sets, conformal maps do indeed extend to the boundary.   Suppose that are bounded, simply connected regions with boundaries and that are simple, continuous, closed curves. Then any conformal map of that is one-to-one onto can be extended to a continuous map of one-to-one onto .    Find a bijective conformal map that takes a bounded region to an unbounded region. Show that there is no bijective conformal map from a simply connected domain to a non-simply connected domain.   "
+  "body": " Simply connected sets, boundaries, and the Riemann mapping theorem  Before we go on, we're going to need to deal with the issue that analytic maps don't always act nicely on the boundaries of their domains. (Remember, analytic and holomorphic are local properties defined on open sets, and the edge of a set isn't open.) Let us recall that a set is called simply connected if it has no holes in it . This intuitive idea of no holes corresponds to the rigorous notion a simply connected set is one in which every simple closed curve can be continuously deformed into a point without leaving the set. The act of continuously deforming a curve within a set is called homotopy . Two curves are said to be homotopic if we can find a homotopy from one curve to the other. More precisely, a homotopy in a domain from a curve to a a curve is a continuous function where for all , and the endpoints have , and .  Two domains are said to be conformally equivalent if there exists a bijective conformal map . The next theorem characterizes which sets are conformally equivalent to simply connected sets.   Riemann Mapping Theorem  Let be a simply connected domain in with . Then there exists a bijective conformal map , where is the complex unit disk. For any fixed , we can find a unique such so that and .   Because the maps in the theorem above are bijections, we get a striking immediate corollary.   Any two simply connected regions, neither one of which is all of , are conformally equivalent.   This is a powerful fact! Unfortunately, this theorem doesn't tell us how to fund the conformal maps that connect different sets. We'll take a deeper dive into that in the next couple of lectures.  Now, we'll turn to the question of what happens to the boundaries of domains under conformal maps. One way to think of the boundary of a domain in is the set of points that can be reached as limits of sequences in . Another is to think of it as the set of points for which any neighborhood contains points in and points outside of . We use the notation to indicated the boundary of . Because domains in are open, a set and its boundary are disjoint. The closure of a domain is and will be denoted (we can also use , but to avoid confusion with the complex conjugate, we'll avoid that in these notes).   Let be domains in with boundaries . Suppose that is conformal (that is, is conformal from its domain to its range). If has boundary and additionally for some , then .   Essentially, the previous proposition implies that we can find the image of a conformal map by computing the image of the boundary and keeping track of where the interior of the domain was mapped via a single point.  We now come to the question of when a conformal map between domains can be extended to the boundary. Note that this extension will be a continuous extension, not an analytic one, because we can't work on neighborhoods for points in the boundary. For nice sets, conformal maps do indeed extend to the boundary.   Suppose that are bounded, simply connected regions with boundaries and that are simple, continuous, closed curves (these are sometimes called Jordan curves). Then any conformal map of that is one-to-one onto can be extended to a continuous map of one-to-one onto .    Find a bijective conformal map that takes a bounded region to an unbounded region. Show that there is no bijective conformal map from a simply connected domain to a non-simply connected domain.   "
 },
 {
   "id": "p-23",
@@ -205,7 +205,7 @@ var ptx_lunr_docs = [
   "type": "Theorem",
   "number": "1.2.4",
   "title": "",
-  "body": " Suppose that are bounded, simply connected regions with boundaries and that are simple, continuous, closed curves. Then any conformal map of that is one-to-one onto can be extended to a continuous map of one-to-one onto .  "
+  "body": " Suppose that are bounded, simply connected regions with boundaries and that are simple, continuous, closed curves (these are sometimes called Jordan curves). Then any conformal map of that is one-to-one onto can be extended to a continuous map of one-to-one onto .  "
 },
 {
   "id": "question-1",
@@ -258,71 +258,44 @@ var ptx_lunr_docs = [
   "url": "section-4.html",
   "type": "Section",
   "number": "1.4",
-  "title": "Exercise set 1",
-  "body": " Exercise set 1   Let be the square with corners . Find the image of the square under the map . Where does the interior of the square map? Is the image a square?  Let be the unit square in . Find the image of the square under the map . Make sure to indicate where the interior maps.  Find the image of the first quadrant under the map .  Let . Plot the set . Find the image of the set under the map .  Near which points are the following functions conformal?   ;     .    Let be constant. Show that defined by can be written as a rotation followed by a magnification followed by a translation.  The Riemann mapping theorem concerns sets that are not all of . Is there a conformal map from that is one-to-one onto the unit disk ? Is there a conformal map of one-to-one onto ?   "
+  "title": "Interlude: Liouville's Theorem",
+  "body": " Interlude: Liouville's Theorem  In the calculus of real functions, it isn't at all unusual to deal with power series that converge everywhere in that represent bounded functions. (A whole class of these in the real world are signals that can be expressed as Fourier series). For example, has a power series representation valid for all real and it bounded in absolute value by 1. The extension theorem tells us that this same series should have infinite radius of convergence on as well, but we should note what happens to the size of the values of the function. Let . Then and which will be unbounded as we let . So even though remains a function with a convergent power series representation on (a so-called entire function), it is an unbounded function. Liouville's theorem asserts that this is the general situation - an entire function that is bounded must necessarily be a constant function.  The proof of Liouville's theorem follows from a useful set of estimates about the behavior of derivatives of bounded analytic functions called the Cauchy inequalities. First, recall that Cauchy's integral theorem for derivatives gives where is a simple closed curve about inside the domain of .   Cauchy inequalities  Let be an analytic function on a domain with bound on , and let be a circle of radius centered at a point with . Then    Suppose that parametrizes . By the integral theorem for derivatives, we have where the inequality follows from the ML-inequality.    Liouville's Theorem  A bounded entire function is necessarily constant.   "
 },
 {
-  "id": "exercise-1",
+  "id": "p-41",
   "level": "2",
-  "url": "section-4.html#exercise-1",
-  "type": "Exercise",
+  "url": "section-4.html#p-41",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "entire "
+},
+{
+  "id": "theorem-8",
+  "level": "2",
+  "url": "section-4.html#theorem-8",
+  "type": "Theorem",
+  "number": "1.4.1",
+  "title": "Cauchy inequalities.",
+  "body": " Cauchy inequalities  Let be an analytic function on a domain with bound on , and let be a circle of radius centered at a point with . Then  "
+},
+{
+  "id": "proof-4",
+  "level": "2",
+  "url": "section-4.html#proof-4",
+  "type": "Proof",
   "number": "1.4.1",
   "title": "",
-  "body": "Let be the square with corners . Find the image of the square under the map . Where does the interior of the square map? Is the image a square? "
+  "body": " Suppose that parametrizes . By the integral theorem for derivatives, we have where the inequality follows from the ML-inequality.  "
 },
 {
-  "id": "exercise-2",
+  "id": "theorem-9",
   "level": "2",
-  "url": "section-4.html#exercise-2",
-  "type": "Exercise",
+  "url": "section-4.html#theorem-9",
+  "type": "Theorem",
   "number": "1.4.2",
-  "title": "",
-  "body": "Let be the unit square in . Find the image of the square under the map . Make sure to indicate where the interior maps. "
-},
-{
-  "id": "exercise-3",
-  "level": "2",
-  "url": "section-4.html#exercise-3",
-  "type": "Exercise",
-  "number": "1.4.3",
-  "title": "",
-  "body": "Find the image of the first quadrant under the map . "
-},
-{
-  "id": "exercise-4",
-  "level": "2",
-  "url": "section-4.html#exercise-4",
-  "type": "Exercise",
-  "number": "1.4.4",
-  "title": "",
-  "body": "Let . Plot the set . Find the image of the set under the map . "
-},
-{
-  "id": "exercise-5",
-  "level": "2",
-  "url": "section-4.html#exercise-5",
-  "type": "Exercise",
-  "number": "1.4.5",
-  "title": "",
-  "body": "Near which points are the following functions conformal?   ;     .   "
-},
-{
-  "id": "exercise-6",
-  "level": "2",
-  "url": "section-4.html#exercise-6",
-  "type": "Exercise",
-  "number": "1.4.6",
-  "title": "",
-  "body": "Let be constant. Show that defined by can be written as a rotation followed by a magnification followed by a translation. "
-},
-{
-  "id": "exercise-7",
-  "level": "2",
-  "url": "section-4.html#exercise-7",
-  "type": "Exercise",
-  "number": "1.4.7",
-  "title": "",
-  "body": "The Riemann mapping theorem concerns sets that are not all of . Is there a conformal map from that is one-to-one onto the unit disk ? Is there a conformal map of one-to-one onto ? "
+  "title": "Liouville's Theorem.",
+  "body": " Liouville's Theorem  A bounded entire function is necessarily constant.  "
 },
 {
   "id": "section-5",
@@ -330,24 +303,96 @@ var ptx_lunr_docs = [
   "url": "section-5.html",
   "type": "Section",
   "number": "1.5",
+  "title": "Exercise set 1",
+  "body": " Exercise set 1   Let be the square with corners . Find the image of the square under the map . Where does the interior of the square map? Is the image a square?  Let be the unit square in . Find the image of the square under the map . Make sure to indicate where the interior maps.  Find the image of the first quadrant under the map .  Let . Plot the set . Find the image of the set under the map .  Near which points are the following functions conformal?   ;     .    Let be constant. Show that defined by can be written as a rotation followed by a magnification followed by a translation.  The Riemann mapping theorem concerns simply connected sets that are not all of . Is there a conformal map from that is one-to-one onto the unit disk ? Is there a conformal map of one-to-one onto ?   "
+},
+{
+  "id": "exercise-1",
+  "level": "2",
+  "url": "section-5.html#exercise-1",
+  "type": "Exercise",
+  "number": "1.5.1",
+  "title": "",
+  "body": "Let be the square with corners . Find the image of the square under the map . Where does the interior of the square map? Is the image a square? "
+},
+{
+  "id": "exercise-2",
+  "level": "2",
+  "url": "section-5.html#exercise-2",
+  "type": "Exercise",
+  "number": "1.5.2",
+  "title": "",
+  "body": "Let be the unit square in . Find the image of the square under the map . Make sure to indicate where the interior maps. "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "section-5.html#exercise-3",
+  "type": "Exercise",
+  "number": "1.5.3",
+  "title": "",
+  "body": "Find the image of the first quadrant under the map . "
+},
+{
+  "id": "exercise-4",
+  "level": "2",
+  "url": "section-5.html#exercise-4",
+  "type": "Exercise",
+  "number": "1.5.4",
+  "title": "",
+  "body": "Let . Plot the set . Find the image of the set under the map . "
+},
+{
+  "id": "exercise-5",
+  "level": "2",
+  "url": "section-5.html#exercise-5",
+  "type": "Exercise",
+  "number": "1.5.5",
+  "title": "",
+  "body": "Near which points are the following functions conformal?   ;     .   "
+},
+{
+  "id": "exercise-6",
+  "level": "2",
+  "url": "section-5.html#exercise-6",
+  "type": "Exercise",
+  "number": "1.5.6",
+  "title": "",
+  "body": "Let be constant. Show that defined by can be written as a rotation followed by a magnification followed by a translation. "
+},
+{
+  "id": "exercise-7",
+  "level": "2",
+  "url": "section-5.html#exercise-7",
+  "type": "Exercise",
+  "number": "1.5.7",
+  "title": "",
+  "body": "The Riemann mapping theorem concerns simply connected sets that are not all of . Is there a conformal map from that is one-to-one onto the unit disk ? Is there a conformal map of one-to-one onto ? "
+},
+{
+  "id": "section-6",
+  "level": "1",
+  "url": "section-6.html",
+  "type": "Section",
+  "number": "1.6",
   "title": "Linear fractional tranformations",
   "body": " Linear fractional tranformations  A couple of you have asked me how to actually find the conformal maps that are guaranteed to exist by the Riemann Mapping Theorem. For a large class of domains, we can readily (for some value of readily) find these!   A linear fractional transformation is a degree one complex rational function of the form for fixed constants , and where we assume that .    What happens to if ?   We will use the shorthand LFT to stand for linear fractional transformation. From what we learned last term, it should be immediately obvious that an LFT has one pole of order 1 where .  "
 },
 {
   "id": "definition-3",
   "level": "2",
-  "url": "section-5.html#definition-3",
+  "url": "section-6.html#definition-3",
   "type": "Definition",
-  "number": "1.5.1",
+  "number": "1.6.1",
   "title": "",
   "body": " A linear fractional transformation is a degree one complex rational function of the form for fixed constants , and where we assume that .  "
 },
 {
   "id": "question-2",
   "level": "2",
-  "url": "section-5.html#question-2",
+  "url": "section-6.html#question-2",
   "type": "Question",
-  "number": "1.5.2",
+  "number": "1.6.2",
   "title": "",
   "body": " What happens to if ?  "
 }
